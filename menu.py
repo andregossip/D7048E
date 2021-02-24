cf75ea2c924442e23aadb5425c4a70ae1c40645c
carker-7 <57618825+carker-7@users.noreply.github.com>
2021-02-24 16:03:36 +0100

    Update menu.py

--- menu.py
+++ menu.py
@@ -14,20 +14,27 @@ class Window(QMainWindow):
         #self.centralWidget.setAlignment(Qt.AlignCenter | Qt.AlignVCenter)
         #self.setCentralWidget(self.centralWidget)
 
+
         self.playButton = QPushButton(self)
         self.playButton.setText("Choose game")
         self.playButton.move(130,150)
         self.playButton.resize(150, 30)
+        self.playButton.setStyleSheet("background-color : #F9E1E0")
+        QPushButton.setAutoDefault(self.playButton, True)
 
         self.extButton = QPushButton(self)
         self.extButton.setText("Use with external game")
         self.extButton.move(130,200)
         self.extButton.resize(150, 30)
+        self.extButton.setStyleSheet("background-color : #FEADB9")
+        QPushButton.setAutoDefault(self.extButton, True)
 
         self.controlButton = QPushButton(self)
         self.controlButton.setText("Change controls")
         self.controlButton.move(130,250)
         self.controlButton.resize(150, 30)
+        self.controlButton.setStyleSheet("background-color : #BC85A3")
+        QPushButton.setAutoDefault(self.controlButton, True)
                 
         self.closeButton = QPushButton(self)
         self.closeButton.setText("Close") 
@@ -35,6 +42,8 @@ class Window(QMainWindow):
         self.closeButton.setShortcut('ESC')
         self.closeButton.move(130,300)
         self.closeButton.resize(150, 30)
+        self.closeButton.setStyleSheet("background-color : #9799BA")
+        QPushButton.setAutoDefault(self.closeButton, True)
                                                                                                     
  
 if __name__ == "__main__":
