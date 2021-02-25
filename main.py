fc8b8e3092e457974722923b4e4bba20f868165c
Alexandre <achapin@insa-rennes.fr>
2021-02-25 13:38:52 +0100

    Add hand gesture recognition

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cv2
 import mediapipe as mp
-from pynput.keyboard import Key, Controller
+from pynput.keyboard import Key, Controller, Listener
+from pynput import keyboard as kb
 
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
@@ -8,6 +9,8 @@ mp_hands = mp.solutions.hands
 keyboard = Controller()
 currentDirection = ""
 currentDirectionKey = []
+isPaused = False
+isPauseReleased = False
 # For webcam input:
 hands = mp_hands.Hands(
     min_detection_confidence=0.5, min_tracking_confidence=0.5)
@@ -36,6 +39,112 @@ def realeseInput(currentdirection, currentDirectionKeys):
         for buttons in currentDirectionKeys:
             keyboard.release(buttons)
 
+def recognizeLeftHandGesture(landmarks):
+    thumbState = 'UNKNOW'
+    indexFingerState = 'UNKNOW'
+    middleFingerState = 'UNKNOW'
+    ringFingerState = 'UNKNOW'
+    littleFingerState = 'UNKNOW'
+    recognizedHandGesture = None
+
+    pseudoFixKeyPoint = landmarks[2]['x']
+    if (landmarks[3]['x'] < pseudoFixKeyPoint and landmarks[4]['x'] < landmarks[3]['x']):
+        thumbState = 'CLOSE'    
+    elif (pseudoFixKeyPoint < landmarks[3]['x'] and landmarks[3]['x'] < landmarks[4]['x']):
+        thumbState = 'OPEN'    
+
+    pseudoFixKeyPoint = landmarks[6]['y']
+    if (landmarks[7]['y'] < pseudoFixKeyPoint and landmarks[8]['y'] < landmarks[7]['y']):
+        indexFingerState = 'OPEN'    
+    elif (pseudoFixKeyPoint < landmarks[7]['y'] and landmarks[7]['y'] < landmarks[8]['y']):
+        indexFingerState = 'CLOSE'    
+
+    pseudoFixKeyPoint = landmarks[10]['y']
+    if (landmarks[11]['y'] < pseudoFixKeyPoint and landmarks[12]['y'] < landmarks[11]['y']):
+        middleFingerState = 'OPEN'    
+    elif (pseudoFixKeyPoint < landmarks[11]['y'] and landmarks[11]['y'] < landmarks[12]['y']):
+        middleFingerState = 'CLOSE'
+
+    pseudoFixKeyPoint = landmarks[14]['y']
+    if (landmarks[15]['y'] < pseudoFixKeyPoint and landmarks[16]['y'] < landmarks[15]['y']):
+        ringFingerState = 'OPEN'    
+    elif (pseudoFixKeyPoint < landmarks[15]['y'] and landmarks[15]['y'] < landmarks[16]['y']):
+        ringFingerState = 'CLOSE'
+
+    pseudoFixKeyPoint = landmarks[18]['y']
+    if (landmarks[19]['y'] < pseudoFixKeyPoint and landmarks[20]['y'] < landmarks[19]['y']):
+        littleFingerState = 'OPEN'    
+    elif (pseudoFixKeyPoint < landmarks[19]['y'] and landmarks[19]['y'] < landmarks[20]['y']):
+        littleFingerState = 'CLOSE'
+
+    if (thumbState == 'CLOSE' and indexFingerState == 'OPEN' and middleFingerState == 'OPEN' and ringFingerState == 'CLOSE' and littleFingerState == 'CLOSE'):
+        recognizedHandGesture = 2 # "TWO"   
+    elif (thumbState == 'CLOSE' and indexFingerState == 'OPEN' and middleFingerState == 'CLOSE' and ringFingerState == 'CLOSE' and littleFingerState == 'CLOSE'):
+        recognizedHandGesture = 1 # "TWO"   
+    else:
+        recognizedHandGesture = 0 # "UNKNOW"
+    return recognizedHandGesture
+
+def getStructuredLandmarks(landmarks):
+  structuredLandmarks = []
+  for j in range(42):
+    if( j %2 == 1):
+      structuredLandmarks.append({ 'x': landmarks[j - 1], 'y': landmarks[j] })
+  return structuredLandmarks
+
+def recognizeRightHandGesture(landmarks):
+    thumbState = 'UNKNOW'
+    indexFingerState = 'UNKNOW'
+    middleFingerState = 'UNKNOW'
+    ringFingerState = 'UNKNOW'
+    littleFingerState = 'UNKNOW'
+    recognizedHandGesture = None
+
+    pseudoFixKeyPoint = landmarks[2]['x']
+    if (landmarks[3]['x'] > pseudoFixKeyPoint and landmarks[4]['x'] > landmarks[3]['x']):
+        thumbState = 'CLOSE'    
+    elif (pseudoFixKeyPoint > landmarks[3]['x'] and landmarks[3]['x'] > landmarks[4]['x']):
+        thumbState = 'OPEN'    
+
+    pseudoFixKeyPoint = landmarks[6]['y']
+    if (landmarks[7]['y'] < pseudoFixKeyPoint and landmarks[8]['y'] < landmarks[7]['y']):
+        indexFingerState = 'OPEN'    
+    elif (pseudoFixKeyPoint < landmarks[7]['y'] and landmarks[7]['y'] < landmarks[8]['y']):
+        indexFingerState = 'CLOSE'    
+
+    pseudoFixKeyPoint = landmarks[10]['y']
+    if (landmarks[11]['y'] < pseudoFixKeyPoint and landmarks[12]['y'] < landmarks[11]['y']):
+        middleFingerState = 'OPEN'    
+    elif (pseudoFixKeyPoint < landmarks[11]['y'] and landmarks[11]['y'] < landmarks[12]['y']):
+        middleFingerState = 'CLOSE'
+
+    pseudoFixKeyPoint = landmarks[14]['y']
+    if (landmarks[15]['y'] < pseudoFixKeyPoint and landmarks[16]['y'] < landmarks[15]['y']):
+        ringFingerState = 'OPEN'    
+    elif (pseudoFixKeyPoint < landmarks[15]['y'] and landmarks[15]['y'] < landmarks[16]['y']):
+        ringFingerState = 'CLOSE'
+
+    pseudoFixKeyPoint = landmarks[18]['y']
+    if (landmarks[19]['y'] < pseudoFixKeyPoint and landmarks[20]['y'] < landmarks[19]['y']):
+        littleFingerState = 'OPEN'    
+    elif (pseudoFixKeyPoint < landmarks[19]['y'] and landmarks[19]['y'] < landmarks[20]['y']):
+        littleFingerState = 'CLOSE'
+
+    if (thumbState == 'CLOSE' and indexFingerState == 'OPEN' and middleFingerState == 'OPEN' and ringFingerState == 'CLOSE' and littleFingerState == 'CLOSE'):
+        recognizedHandGesture = 2 # "TWO"   
+    elif (thumbState == 'CLOSE' and indexFingerState == 'OPEN' and middleFingerState == 'CLOSE' and ringFingerState == 'CLOSE' and littleFingerState == 'CLOSE'):
+        recognizedHandGesture = 1 # "TWO"   
+    else:
+        recognizedHandGesture = 0 # "UNKNOW"
+    return recognizedHandGesture
+
+def getStructuredLandmarks(landmarks):
+    structuredLandmarks = []
+    for j in range(42):
+        if( j %2 == 1):
+            structuredLandmarks.append({ 'x': landmarks[j - 1], 'y': landmarks[j] })
+    return structuredLandmarks
+
 while cap.isOpened():
   success, image = cap.read()
   if not success:
@@ -59,62 +168,70 @@ while cap.isOpened():
   image.flags.writeable = True
   image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
   if results.multi_hand_landmarks:
-
-
-    #print('index finger', results.multi_hand_landmarks[mp_hands.HandLandmark.MIDDLE_FINGER_MCP].x)
     for hand_landmarks in results.multi_hand_landmarks:
         x = hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_MCP].x * image_width
         y = hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_MCP].y * image_height
         #right
         if x > 420 and (y > 160 and y < 320) and currentDirection != "right":
-            print("Right")
+            #print("Right")
             doInput([Key.right], currentDirection, currentDirectionKey)
             currentDirection = "right"
             currentDirectionKey = [Key.right]
         #left
         elif x < 210 and (y > 160 and y < 320) and currentDirection != "left":
-            print("Left")
+            #print("Left")
             doInput([Key.left], currentDirection, currentDirectionKey)
             currentDirection = "left"
             currentDirectionKey = [Key.left]
         #up
         elif y < 160 and (x < 420 and x > 210) and currentDirection != "up":
-            print("up")
+            #print("up")
             doInput([Key.up], currentDirection, currentDirectionKey)
             currentDirection = "up"
             currentDirectionKey = [Key.up]
         #down
         elif y > 320 and (x < 420 and x > 210) and currentDirection != "down":
-            print("down")
+            #print("down")
             doInput([Key.down], currentDirection, currentDirectionKey)
             currentDirection = "down"
             currentDirectionKey = [Key.down]
         #up and right
         elif y < 160 and x > 420 and currentDirection != "upRight":
-            print("up and right")
+            #print("up and right")
             doInput([Key.up, Key.right], currentDirection, currentDirectionKey)
             currentDirection = "upRight"
             currentDirectionKey = [Key.up, Key.right]
         elif x < 210 and y < 160 and currentDirection != "upLeft":
-            print("up and left")
+            #print("up and left")
             doInput([Key.up, Key.left], currentDirection, currentDirectionKey)
             currentDirection = "upLeft"
             currentDirectionKey = [Key.up, Key.left]
 
         elif (x > 210 and x < 420) and (y > 160 and y < 320) and currentDirection != "":
-            print("Stop")
+            #print("Stop")
             realeseInput(currentDirection, currentDirectionKey)
             currentDirection = ""
             currentDirectionKey = []
         mp_drawing.draw_landmarks(
-          image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
-        #print('hand_landmarks:', hand_landmarks)
-        #print(
-        #  f'Index finger tip coordinates: (',
-        #  f'{hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_MCP].x * image_width}, '
-        #  f'{hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_MCP].y * image_height}')
+        image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+    keypoints = []
+    for landmark in results.multi_hand_landmarks[0].landmark:
+        keypoints.append(landmark.x)
+        keypoints.append(landmark.y)
+        currentDirection = ""
+        currentDirectionKey = []
+    if(recognizeRightHandGesture(getStructuredLandmarks(keypoints)) == 2):
+        togglePause([Key.esc], isPaused)
+
+    with kb.Events() as events:
+        for event in events:
+            if event.key == kb.Key.esc:
+                break
+            else:
+                print('Received event {}'.format(event))
+
   cv2.imshow('MediaPipe Hands', image)
-  if cv2.waitKey(5) & 0xFF == 27:
+  if cv2.waitKey(5) & 0xFF == 9:
     break
 hands.close()
 cap.release()
